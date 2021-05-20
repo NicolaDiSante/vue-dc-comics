11ec1e56f41c0f0878d21965b7ba0247af5a6fc9
NicolaDiSante <nickymacha90@gmail.com>
2021-05-20 16:21:30 +0200

    Header/Main Completato

---
+++ src/components/Header.vue
@@ -0,0 +1,113 @@
+<template>
+  
+  <header class="container">
+
+    <div class="logo">
+      <img src="../assets/img/dc-logo.png" alt="DC Logo">
+    </div>
+
+    <div>
+      <ul>
+        <li v-for="(link, index) in links" :key="index">
+          <a :class="{'active' : link.current}" :href="link.url"> {{ link.text }} </a>
+        </li>
+      </ul>
+    </div>
+
+  </header>
+
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data(){
+    return{
+      links:[
+        {
+          text: 'Characters',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Comics',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Movies',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Tv',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Games',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Collectibles',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Videos',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Fans',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'News',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Shop',
+          url: '#',
+          current: false
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import '@/assets/style/utilities';
+
+header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  padding: 10px 0;
+  li{
+    display: inline;
+    text-transform: uppercase;
+    margin-left: 15px;
+    a{
+      color: black;
+      font-size: 13px;
+      font-weight: 700;
+      padding-bottom: 52px;
+      &:hover,
+      &.active{
+        border-bottom: 3px solid #0c7cec;
+        color: #0c7cec;
+      }
+
+    }
+  }
+}
+
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+
+  <main>
+
+    <div class="container">
+      <h1> --> Content goes here &lt;-- </h1>
+    </div>
+    
+
+  </main>
+  
+
+</template>
+
+<script>
+export default {
+  name: 'Main'
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import '@/assets/style/utilities.scss';
+
+main{
+  display: flex;
+  align-items: center;
+  height: 130px;
+  width: 100%;
+  background-color: black;
+  h1{
+  color: white;
+}
+}
+
+
+</style>
